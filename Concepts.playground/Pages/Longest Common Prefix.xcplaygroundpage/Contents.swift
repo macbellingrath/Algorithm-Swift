feb64fe327d5025509fc729282ed1a9325c61f99
Mac Bellingrath <mbellingrath@weddingwire.com>
2017-09-25 09:33:16 -0400

    adds vertical approach to lcp

--- Concepts.playground/Pages/Longest Common Prefix.xcplaygroundpage/Contents.swift
+++ Concepts.playground/Pages/Longest Common Prefix.xcplaygroundpage/Contents.swift
@@ -7,7 +7,7 @@ import Foundation
  Write a function to find the longest common prefix string amongst an array of strings.
  */
 class Solution {
-    func longestCommonPrefix(_ strs: [String]) -> String {
+    func longestCommonPrefix_Horizontal(_ strs: [String]) -> String {
         if strs.isEmpty {
             return ""
         }
@@ -24,9 +24,27 @@ class Solution {
 
         return String(prefix)
     }
+
+    func longestCommonPrefix_Vertical(_ strs: [String]) -> String {
+        if strs.isEmpty { return "" }
+        if strs.count == 1 { return strs[0] }
+
+        var i = strs[0].startIndex
+
+        for str in strs[1..<strs.count] {
+            while i < strs[0].endIndex && i < str.endIndex && str[i] == strs[0][i] {
+                i = strs[0].index(after: i)
+            }
+        }
+
+        return String(strs[0][strs[0].startIndex..<i])
+    }
 }
 
 let s = Solution()
-print(s.longestCommonPrefix(["car", "cars", "caring"]))
-print(s.longestCommonPrefix(["a","b" ]))
+//print(s.longestCommonPrefix_Horizontal(["car", "cars", "caring"]))
+//print(s.longestCommonPrefix_Horizontal(["a","b" ]))
+
+print(s.longestCommonPrefix_Vertical(["car", "cars", "caring"]))
+print(s.longestCommonPrefix_Vertical(["aa", "a"]))
 //: [Next](@next)
