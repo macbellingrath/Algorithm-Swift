82001447c984f0c4651b12e34be4feb2dfdb125a
Mac Bellingrath <macbellingrath@gmail.com>
2019-08-01 12:16:29 -0400

    [Update] my lru cache is still failing some tests

--- Concepts.playground/Pages/LRU.xcplaygroundpage/Contents.swift
+++ Concepts.playground/Pages/LRU.xcplaygroundpage/Contents.swift
@@ -1,6 +1,3 @@
-//: [Previous](@previous)
-
-import Foundation
 
 /*
  Implement a LRU Cache.
@@ -9,145 +6,123 @@ import Foundation
 
 
 class ListNode {
-    let value: Int
+    var value: Int
 
     var next: ListNode?
     var prev: ListNode?
 
-    init(val: Int) {
-        self.value = val
+    init(_ value: Int) {
+        self.value = value
     }
-
 }
 
 class Queue {
-
     var head: ListNode?
     var tail: ListNode?
     private(set) var count: Int = 0
 
     func enqueue(_ value: Int) {
-        let node = ListNode(val: value)
-        if head == nil {
-            head = node
-            tail = head
-        }
-
-        node.next = head
-        head?.prev = node
-        head = node
         count += 1
+        let node = ListNode(value)
+        let oldTail = tail
+
+        tail = node
+        if head == nil {
+            head = tail
+        } else {
+            oldTail?.next = tail
+            node.prev = oldTail
+        }
     }
 
     func dequeue() -> ListNode? {
-        let temp = head
-        head = head?.next
-        head?.prev = nil
-        temp?.next = nil
-        count = max(0, count - 1)
-        return temp
+        if let oldHead = head {
+            self.head = oldHead.next
+            if oldHead.next == nil {
+                tail = nil
+            }
+            count = max(0, count - 1)
+            return oldHead
+        }
+        return nil
     }
 
     func erase(_ node: ListNode) {
-        if node === head {
-            dequeue()
-        } else if node === tail {
-            popBack()
+        let prev = node.prev
+        let next = node.next
+        if let prev = prev {
+            prev.next = next
         } else {
-            let prev = node.prev
-            let next = node.next
-            prev?.next = next
-
-            node.prev = nil
-            node.next = nil
-
-            count = max(0, count - 1)
+            head = next
         }
+        next?.prev = prev
+
+        node.next = nil
+        node.prev = nil
+        count = max(0, count - 1)
     }
 
     func popBack() -> ListNode? {
-        let temp = tail
-        tail?.prev?.next = nil
-        tail = tail?.prev
+        if let oldTail = tail {
+            tail = tail?.prev
+            tail?.next = nil
 
-        temp?.next = nil
-        temp?.prev = nil
-        count = max(0, count - 1)
-        return temp
+            oldTail.prev = nil
+
+            if head === tail {
+                head = nil
+            }
+            oldTail.next = nil
+            count -= 1
+            return oldTail
+        }
+        return nil
     }
 }
-
-extension Queue: CustomPlaygroundDisplayConvertible {
-    var playgroundDescription: Any {
-        return "queue count - \(count), head: \(head?.value ?? -1), tail: \(tail?.value ?? -1)"
-    }
-}
-
 
 
 class LRUCache {
     private let capacity: Int
-    private var hashTable: [Int: ListNode] = [:]
+    private var cache: [Int: Int] = [:]
+    private var keyToNode: [Int: ListNode] = [:]
     private let queue = Queue()
 
     init(_ capacity: Int) {
         self.capacity = capacity
     }
 
-    func get(_ key: Int) -> Int {
-        if let node = hashTable[key] {
-            // move to front
-            queue.erase(node)
-            queue.enqueue(node.value)
-            hashTable[key] = queue.head
 
-            return node.value
+    func get(_ key: Int) -> Int {
+        guard let value = cache[key] else {
+            return -1
         }
-        return -1
+        remove(key)
+        insert(key, value: value)
+        return value
+    }
+
+    private func insert(_ key: Int, value: Int) {
+        cache[key] = value
+        queue.enqueue(key)
+        keyToNode[key] = queue.head
+    }
+
+    private func remove(_ key: Int) {
+        cache.removeValue(forKey: key)
+        guard let node = keyToNode[key] else {
+            return
+        }
+        queue.erase(node)
+        keyToNode.removeValue(forKey: key)
     }
 
     func put(_ key: Int, _ value: Int) {
-        if get(key) == -1 {
-            if queue.count == capacity {
-                if let last = queue.popBack() {
-                    print("evicting \(last.value)")
-                    for (key, value) in hashTable {
-                        if value === last {
-                            hashTable.removeValue(forKey: key)
-                            break
-                        }
-                    }
-                    hashTable.removeValue(forKey: last.value)
-                }
-            }
-            queue.enqueue(value)
-            hashTable[key] = queue.head
+        if cache[key] != nil {
+            remove(key)
+        } else if queue.count == capacity, let keyToRemove = queue.tail?.value {
+            remove(keyToRemove)
         }
+        insert(key, value: value)
     }
 
 }
-
-// Test
-let lru = LRUCache(2)
-// put [1,1]
-lru.put(1, 9)
-// put [2,2]
-lru.put(2, 18)
-// get 1
-lru.get(1) == 9
-// put [3,3]
-lru.put(3, 27)
-
-// get 2
-lru.get(2) == -1
-// put [4,4]
-lru.put(4, 36)
-// get 1
-lru.get(1) == -1
-// get 3
-lru.get(3) == 27
-// get 4
-lru.get(4) == 36
-
-
-//: [Next](@next)
