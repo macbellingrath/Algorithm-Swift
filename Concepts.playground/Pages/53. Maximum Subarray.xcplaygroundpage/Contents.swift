64fb9ea187ee3df84eeac802d83576c94c4be4f3
Mac Bellingrath <macbellingrath@gmail.com>
2019-10-11 15:39:13 -0400

    [Add] adds 53. Maximum Subarray

---
+++ Concepts.playground/Pages/53. Maximum Subarray.xcplaygroundpage/Contents.swift
@@ -0,0 +1,30 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*
+ Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+ Input: [-2,1,-3,4,-1,2,1,-5,4],
+ Output: 6
+ Explanation: [4,-1,2,1] has the largest sum = 6.
+ */
+
+class Solution {
+    func maxSubArray(_ nums: [Int]) -> Int {
+        guard !nums.isEmpty else { return .min }
+
+        var dp = [nums[0]]
+        var i = 1
+        while i < nums.count {
+          dp.append(max(dp[i - 1] + nums[i], nums[i]))
+          i += 1
+        }
+
+        return dp.max()!
+    }
+}
+
+
+
+//: [Next](@next)
